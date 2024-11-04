4a9a2a7d396651d4428d6e7b767ae1c5c50031d5
EstebanAaron <lr96aron@gmail.com>
2024-11-04 17:16:00 +0000

    save method

--- src/StoreFile.php
+++ src/StoreFile.php
@@ -1,11 +1,16 @@
 <?php
  namespace Daw2\Dsw2024TiendaForm;
 
+use DateTime;
+
  class StoreFile implements StoreInterface{
   
   public function addRate(int $rate)
   {
-    
+    $rate =$_POST['rate'];
+    $date = new DateTime();
+    $fileName='../public/rates/'.$date->format('Y-m-d-H-i').'.csv';
+    file_put_contents($fileName,$rate.', ', FILE_APPEND );
   }
 
   public function getStadistics(): array
